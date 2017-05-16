5fdc4fa01d82d6ced0ea6a3e61b1447e35035a76
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-05-16 11:46:01 +0200

    script for parsing log

---
+++ scripts/pythonScript/DirWalk.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+import parser
+
+logfilename = 'stdout.log'
+
+def visit(walk_dir):
+    totallog = 0
+    nr_errors = 0
+    logwithoutpatch = []
+    jsonfilename = "results.json"
+    print('walk_dir = ' + walk_dir)
+
+    print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+
+    for root, subdirs, files in os.walk(walk_dir):
+        print('--\nroot = ' + root)
+
+        for filename in files:
+
+            file_path = os.path.join(root, filename)
+
+            if filename == logfilename:
+                try:
+                    print(filename)
+                    file = open(file_path, 'r').read()
+                    slittedLog = file.split(' --SOLUTIONS DESCRIPTION--')
+                    if (len(slittedLog) > 1):
+                        jsonresults = parser.parseLog(slittedLog[1], root)
+                        if jsonresults is not None:
+                            saveJSONOnFIle(root, jsonresults, jsonfilename)
+                            totallog+=1
+                        else:
+                            nr_errors += 1
+
+                    else:
+                        logwithoutpatch.append(file_path)
+                except Exception as s:
+                    print("error {}".format(file_path))
+                    print(str(s))
+                    return
+
+
+    print("Total {} files:  {} with any patch: {} ".format(logfilename,totallog,nr_errors))
+    print("With no patch: ")
+    print(*logwithoutpatch, sep="\n")
+
+def saveJSONOnFIle(root, results, filename= "test1.json"):
+    path = os.path.join(root, filename)  # scope
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    file = open(path, "w")
+    file.write(json.dumps(results, indent=4, sort_keys=True))
+    file.close()
---
+++ scripts/pythonScript/ParserTest.py
@@ -0,0 +1,65 @@
+import unittest
+from parser import *
+
+class JSONParserTest(unittest.TestCase):
+
+
+    def test_parserS1M40(self):
+        content = getLogContent(root="./data/", filename="stdout_seed1M40.log")
+        json = parseLog(root="./data/", log=content)
+        patches = json["patches"]
+        self.assertEqual(4, len(patches)) ##3 patches discard
+
+        for p in json["patches"]: ##we dont analyze this variants (failures in regression)
+            self.assertNotEquals(p["variant"], 1600)
+            self.assertNotEquals(p["variant"], 578)
+
+
+        patch0Results = patches[0]["patchvalidation"]
+        self.assertEqual(patches[0]["variant"], 292)
+        self.assertEqual(int(patch0Results["numberEvosuiteFailing"]), 3)
+        self.assertEqual(int(patch0Results['numberAllEvosuiteTestGenerated']), 28)
+        self.assertEqual(len(patch0Results["evoFailingTest"]), 3)
+        self.assertEqual(int(patch0Results["evoFailingTest"][0]["failingTestName"]), 14)
+        self.assertEqual((patch0Results["evoFailingTest"][0]["failingClassName"]), 'org.apache.commons.math.analysis.solvers.BracketingNthOrderBrentSolver_ESTest')
+        self.assertEqual(int(patch0Results["evoFailingTest"][1]["failingTestName"]), 16)
+        self.assertEqual(int(patch0Results["evoFailingTest"][2]["failingTestName"]), 17)
+
+
+
+        patch0Results = patches[1]["patchvalidation"]
+        self.assertEqual(patches[1]["variant"], 1024)
+        self.assertEqual(int(patch0Results["numberEvosuiteFailing"]), 2)
+        self.assertEqual(int(patch0Results['numberAllEvosuiteTestGenerated']), 28)
+        self.assertEqual(len(patch0Results["evoFailingTest"]), 2)
+        self.assertEqual(int(patch0Results["evoFailingTest"][0]["failingTestName"]), 16)
+        self.assertEqual((patch0Results["evoFailingTest"][0]["failingClassName"]), 'org.apache.commons.math.analysis.solvers.BracketingNthOrderBrentSolver_ESTest')
+        self.assertEqual(int(patch0Results["evoFailingTest"][1]["failingTestName"]), 2)
+
+    def test_parserS1C25(self):
+        content = getLogContent(root="./data/", filename="stdout_seed1_c25.log")
+        json = parseLog(root="./data/", log=content)
+        patches = json["patches"]
+        self.assertEqual(7, len(patches))  ##3 patches discard
+
+        for p in json["patches"]:  ##we dont analyze this variants (failures in regression)
+            self.assertNotEquals(p["variant"], 28)
+
+
+        patch0Results = patches[0]["patchvalidation"]
+        self.assertEqual(patches[0]["variant"], 142)
+        self.assertEqual(int(patch0Results["numberEvosuiteFailing"]), 4)
+        self.assertEqual(int(patch0Results['numberAllEvosuiteTestGenerated']), 67)
+        self.assertEqual(len(patch0Results["evoFailingTest"]), 4)
+        self.assertEqual(int(patch0Results["evoFailingTest"][0]["failingTestName"]), 18)
+        self.assertEqual((patch0Results["evoFailingTest"][0]["failingClassName"]),
+                         'org.jfree.data.statistics.DefaultStatisticalCategoryDataset_ESTest')
+        self.assertEqual(int(patch0Results["evoFailingTest"][1]["failingTestName"]), 54)
+        self.assertEqual(int(patch0Results["evoFailingTest"][2]["failingTestName"]), 5)
+        self.assertEqual(int(patch0Results["evoFailingTest"][2]["failingTestName"]), 4)
+
+        patch0Results = patches[1]["patchvalidation"]
+        self.assertEqual(patches[1]["variant"], 300)
+        self.assertEqual(int(patch0Results["numberEvosuiteFailing"]), 0)
+        self.assertEqual(int(patch0Results['numberAllEvosuiteTestGenerated']), 243)
+        self.assertEqual(len(patch0Results["evoFailingTest"]), 0)
