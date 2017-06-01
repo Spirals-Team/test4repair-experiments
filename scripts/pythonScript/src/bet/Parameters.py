842ce68274bf918956b912bceade929e91252306
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-06-01 15:39:26 +0200

    Reorganizing scripts + readme

---
+++ scripts/pythonScript/src/bet/Parameters.py
@@ -0,0 +1,4 @@
+pathToTest4RepairRoot = "../../../../../"
+JSONASTORFolder = "/results/jGenProg+MinImpact/Patches and Analysis/"
+JSONBETFolder = "/results/astor-test-and-classification/"
+patchClassificationFolder = "/results/jGenProg+MinImpact/PatchesOverfittingClassification/"
